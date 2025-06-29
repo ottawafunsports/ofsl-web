b774ad041431fc1649551509290733c97dba8492
E ZED <hzhang83@gmail.com>
2025-06-29 12:43:27 -0400

    Fix duplicate import in LeagueEditPage

--- src/screens/MyAccount/components/LeagueEditPage.tsx
+++ src/screens/MyAccount/components/LeagueEditPage.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate, Link } from 'react-router-dom';
 import { getStripeProductByLeagueId, updateStripeProductLeagueId } from '../../../lib/stripe';
-import { getStripeProductByLeagueId, updateStripeProductLeagueId } from '../../../lib/stripe';
 import { Button } from '../../../components/ui/button';
 import { Card, CardContent } from '../../../components/ui/card';
 import { Input } from '../../../components/ui/input';
