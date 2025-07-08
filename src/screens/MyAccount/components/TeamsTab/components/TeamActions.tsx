b400d07df1792f2168310981311471e4c71a01c2
ofsl <ottawafunsports@gmail.com>
2025-07-08 08:10:39 -0400

    Update Team Button Text

--- src/screens/MyAccount/components/TeamsTab/components/TeamActions.tsx
+++ src/screens/MyAccount/components/TeamsTab/components/TeamActions.tsx
@@ -84,7 +84,7 @@ export function TeamActions({
                   <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path>
                   <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
                 </svg>
-                Leave Team
+                Unregister
               </>
             ) : (
               <>
@@ -114,7 +114,7 @@ export function TeamActions({
                   <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path>
                   <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
                 </svg>
-               Leave Team
+               Unregister
               </>
             )}
           </Button>
